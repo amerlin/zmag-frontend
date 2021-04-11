8293f57eaff8ca7999254e664f8479881760f178
Andrea <andrea.merlin@aalto.it>
2021-04-11 23:24:19 +0200

    modifica della side bar

--- src/components/orders/orders.jsx
+++ src/components/orders/orders.jsx
@@ -5,18 +5,22 @@ const Orders = () => {
   return (
     <div>
       <h2>Vendita</h2>
-      <Button variant="primary">Selezionare il cliente</Button>&nbsp;
+      <Button variant="primary">1) Selezionare il cliente</Button>&nbsp;
       <p>
         Una volta aperto il popup e selezionato il cliente, impostare i dati del
         cliente selezionato in una serie di campi non modificabili
       </p>
       <p>FORM CON I DATI DEL CLIENTE</p>
-      <Button variant="primary">Aggiungi prodotto</Button>
+      <Button variant="primary">2) Aggiungi prodotto</Button>
       <p>
         Una volta aperto il popup e selezionato il prodotto con la quantità,
         aggiungere i dati nella nella griglia
       </p>
       <p>GRIGLIA CON I PRODOTTI SELEZIONATI</p>
+      <Button variant="primary">
+        Salvataggio dell'ordine facendo una post
+      </Button>
+      &nbsp;
     </div>
   );
 };
