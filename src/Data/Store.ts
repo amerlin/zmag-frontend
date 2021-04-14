4719060b7283b07b1635bb847326c194ffa15a4f
Andrea <andrea.merlin@aalto.it>
2021-04-14 13:45:21 +0200

    creazione dello store

---
+++ src/Data/Store.ts
@@ -0,0 +1,9 @@
+//Inserisco qui redxu
+
+interface Store {
+  loading: boolean;
+  products: ProductData[];
+  selectedProduct: ProductData;
+  showModalProduct: boolean;
+  showModalCustomer: boolean;
+}
