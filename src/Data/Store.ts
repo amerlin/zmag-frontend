ebfe52ae7fb49033117bcb93a84e977e66e5d445
Andrea <andrea.merlin@aalto.it>
2021-04-14 14:59:58 +0200

    creazione dello storage

--- src/Data/Store.ts
+++ src/Data/Store.ts
@@ -1,9 +1,111 @@
-//Inserisco qui redxu
+import { Store, createStore, combineReducers } from "redux";
+import { ProductData } from "./ProductData";
+import { CustomerData } from "./CustomerData";
 
-interface Store {
-  loading: boolean;
-  products: ProductData[];
-  selectedProduct: ProductData;
-  showModalProduct: boolean;
-  showModalCustomer: boolean;
+//GeneralState
+interface GeneralState {
+  readonly loading: boolean;
+  readonly showModalProduct: boolean;
+  readonly showModalCustomer: boolean;
+  readonly products: ProductData[];
+  readonly customers: CustomerData[];
+  readonly selectedProduct: ProductData | null;
+  readonly selectedCustomer: CustomerData | null;
+}
+
+//Initial
+const initialGeneralState: GeneralState = {
+  loading: false,
+  products: [],
+  customers: [],
+  selectedProduct: null,
+  selectedCustomer: null,
+  showModalProduct: false,
+  showModalCustomer: false,
+};
+
+//AppState
+export interface AppState {
+  readonly zmagState: GeneralState;
+}
+
+//Get products
+export const GETTINGPRODUCTS = "GettingProducts";
+export const gettingProductsAction = () =>
+  ({
+    type: GETTINGPRODUCTS,
+  } as const);
+
+//Got products
+export const GOTPRODUCTS = "GotProducts";
+export const gotProductsAction = (products: ProductData[]) =>
+  ({
+    type: GOTPRODUCTS,
+    products: products,
+  } as const);
+
+//Get product
+export const GETTINGPRODUCT = "GettingProduct";
+export const gettingProductAction = () =>
+  ({
+    type: GETTINGPRODUCT,
+  } as const);
+
+//Got product
+export const GOTPRODUCT = "GotProduct";
+export const gotProductAction = (product: ProductData | null) =>
+  ({
+    type: GOTPRODUCT,
+    product: product,
+  } as const);
+
+//Action type
+type ZmagActions =
+  | ReturnType<typeof gettingProductsAction>
+  | ReturnType<typeof gotProductsAction>
+  | ReturnType<typeof gettingProductAction>
+  | ReturnType<typeof gotProductAction>;
+
+//Reducer
+const ZmagReducer = (state = initialGeneralState, action: ZmagActions) => {
+  switch (action.type) {
+    case GETTINGPRODUCTS: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case GOTPRODUCTS: {
+      return {
+        ...state,
+        products: action.products,
+        loading: false,
+      };
+    }
+    case GETTINGPRODUCT: {
+      return {
+        ...state,
+        selectedProduct: null,
+        loading: true,
+      };
+    }
+    case GOTPRODUCT: {
+      return {
+        ...state,
+        selectedProduct: action.product,
+        loading: false,
+      };
+    }
+  }
+};
+
+//root creation
+const rootReducer = combineReducers<AppState>({
+  zmagState: ZmagReducer,
+});
+
+//reducer creation
+export function configureStore(): Store<AppState> {
+  const store = createStore(rootReducer, undefined);
+  return store;
 }
